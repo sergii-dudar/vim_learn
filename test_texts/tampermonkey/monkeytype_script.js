f820f70c7f93e2324bfa3ed4af3f3b41ffb6babf
Serhii.Dudar <sergey.dudar@sweetcode.pro>
2024-01-16 19:06:53 +0200

    tampermonkey scripts update

--- test_texts/tampermonkey/monkeytype_script.js
+++ test_texts/tampermonkey/monkeytype_script.js
@@ -9,49 +9,49 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
   'use strict';
 
   setTimeout(function () {
 
-      document.addEventListener('keydown', function keydownHandler(event) {
-          if (event.key === 'Tab') {
-              keydownHandler.isTabPressed = true;
-          } else if (event.key === 'Enter' && keydownHandler.isTabPressed) {
-              keydownHandler.isTabPressed = false;
-              incrementIfPassed();
-          }
-      });
-
-      function incrementIfPassed() {
-
-          let resultElement = document.getElementById("result");
-          if (resultElement.className === 'hidden') {
-              console.log("Need pass training to increment counter, skipped!");
-          } else {
-              let counterEl = document.getElementById("custom_counter");
-              counterEl.innerText = Number(counterEl.innerText) + 1;
-          }
+    document.addEventListener('keydown', function keydownHandler(event) {
+      if (event.key === 'Tab') {
+        keydownHandler.isTabPressed = true;
+      } else if (event.key === 'Enter' && keydownHandler.isTabPressed) {
+        keydownHandler.isTabPressed = false;
+        incrementIfPassed();
       }
+    });
 
-      registerCustomCounter();
+    function incrementIfPassed() {
 
-      function registerCustomCounter() {
-          setTimeout(function () {
-
-              let firstButton = document.getElementById('startTestButton');
-
-              let nextDiv = Array.from(firstButton.parentElement.children)
-              .find(value => value.nodeName === 'DIV');
-
-              let counterParent = document.createElement('a');
-              counterParent.className = 'textButton';
-              counterParent.innerHTML = 'Counter:&nbsp;<b id="custom_counter">0</b>'
-              nextDiv.insertAdjacentElement("beforebegin", counterParent)
-
-              console.log("Register counter!");
-          }, 500);
+      let resultElement = document.getElementById("result");
+      if (resultElement.className === 'hidden') {
+        console.log("Need pass training to increment counter, skipped!");
+      } else {
+        let counterEl = document.getElementById("custom_counter");
+        counterEl.innerText = Number(counterEl.innerText) + 1;
       }
+    }
+
+    registerCustomCounter();
+
+    function registerCustomCounter() {
+      setTimeout(function () {
+
+        let firstButton = document.getElementById('startTestButton');
+
+        let nextDiv = Array.from(firstButton.parentElement.children)
+          .find(value => value.nodeName === 'DIV');
+
+        let counterParent = document.createElement('a');
+        counterParent.className = 'textButton';
+        counterParent.innerHTML = 'Counter:&nbsp;<b id="custom_counter">0</b>'
+        nextDiv.insertAdjacentElement("beforebegin", counterParent)
+
+        console.log("Register counter!");
+      }, 500);
+    }
   }, 500);
 
 })();
--- test_texts/tampermonkey/vim_hero_script.js
+++ test_texts/tampermonkey/vim_hero_script.js
@@ -9,53 +9,53 @@
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
   'use strict';
 
   setTimeout(function () {
-      document.addEventListener('keydown', function (event) {
-          if (event.key === 'r') {
-              incrementIfPassed();
-          }
+    document.addEventListener('keydown', function (event) {
+      if (event.key === 'r') {
+        incrementIfPassed();
+      }
+    });
+
+    function incrementIfPassed() {
+      if (!document.getElementsByClassName('prism-code').length) {
+        let counterEl = document.getElementById("custom_counter");
+        counterEl.innerText = Number(counterEl.innerText) + 1;
+      } else {
+        console.log("Need pass training to increment counter, skipped!");
+      }
+    }
+
+    registerCustomCounterElListeners(
+      Array.from(document.querySelectorAll(".menu li a")));
+
+    registerCustomCounter();
+
+    function registerCustomCounterElListeners(els) {
+      els.forEach(el => {
+        el.addEventListener('click', registerCustomCounter);
       });
+    }
 
-      function incrementIfPassed() {
-          if (!document.getElementsByClassName('prism-code').length) {
-              let counterEl = document.getElementById("custom_counter");
-              counterEl.innerText = Number(counterEl.innerText) + 1;
-          } else {
-              console.log("Need pass training to increment counter, skipped!");
-          }
-      }
+    function registerCustomCounter() {
+      setTimeout(function () {
+        registerCustomCounterElListeners(
+          Array.from(document.querySelectorAll(".justify-between a")));
 
-      registerCustomCounterElListeners(
-          Array.from(document.querySelectorAll(".menu li a")));
+        document.querySelector("button.badge").addEventListener('click', function () {
+          incrementIfPassed();
+        });
 
-      registerCustomCounter();
-
-      function registerCustomCounterElListeners(els) {
-          els.forEach(el => {
-              el.addEventListener('click', registerCustomCounter);
-          });
-      }
-
-      function registerCustomCounter() {
-          setTimeout(function () {
-              registerCustomCounterElListeners(
-                  Array.from(document.querySelectorAll(".justify-between a")));
-
-              document.querySelector("button.badge").addEventListener('click', function () {
-                  incrementIfPassed();
-              });
-
-              let tabsContainers = document.getElementsByClassName("bg-neutral-focus");
-              if (tabsContainers.length) {
-                  let tabsContainer = tabsContainers[0];
-                  tabsContainer.innerHTML += '<a class="tab">Counter:&nbsp;<b id="custom_counter">0</b></a>';
-              }
-              console.log("Register counter!");
-          }, 500);
-      }
+        let tabsContainers = document.getElementsByClassName("bg-neutral-focus");
+        if (tabsContainers.length) {
+          let tabsContainer = tabsContainers[0];
+          tabsContainer.innerHTML += '<a class="tab">Counter:&nbsp;<b id="custom_counter">0</b></a>';
+        }
+        console.log("Register counter!");
+      }, 500);
+    }
   }, 500);
 
 })();
